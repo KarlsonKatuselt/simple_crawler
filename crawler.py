e3d2722c19ccc5cc91ec22ffae3db3af3ef2ffc4
KarlsonKatuselt <karlmeelis@gmail.com>
2017-01-18 18:45:54 +0000

    some cleaning

--- crawler.py
+++ crawler.py
@@ -1,17 +1,24 @@
 import bs4 as bs
 import urllib.request
 
-def search(searchee = '960m'):
+def url(searchee = '960m'):
     result = urllib.request.urlopen('http://www.ebay.co.uk/sch/PC-Laptops-Netbooks/177/i.html?_sop=15&_from=R40&_oac=1&_nkw={0}&_ipg=200&rt=nc'.format(searchee)).read()
+    resultFile = open('search_result_{0}.txt'.format(searchee), 'a')
+    return result, resultFile
+
+def crawl_for_urls():
+    result, resultFile =  url()
     soup = bs.BeautifulSoup(result, 'lxml')
-    input('press any key to finish')
-    resultFile = open('search_result{0}.txt'.format(searchee), 'a')
-    links = soup.find_all('h3', class_='lvtitle')
-    for i in links:
-        appendMe = "\n" + str(i)
-        resultFile.write(appendMe)
-    resultFile.write(str(links))
+    laptops = soup.find_all('h3', class_ = 'lvtitle')
+    links = []
+    
+    for i in laptops:
+        links.append(i.a('href'))
+        print(i.a('href'))
+        type(i.a('href'))
+
     resultFile.close()
+    print(str(links))
 
 
-search()
+crawl_for_urls()
